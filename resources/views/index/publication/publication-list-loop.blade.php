08cff5dc5e7585a952eed5a89e63db70bd4fbcd5
abdi_arman <arman.abdiyev@gmail.com>
2019-04-15 22:32:55 +0600

    commit

--- resources/views/index/publication/publication-list-loop.blade.php
+++ resources/views/index/publication/publication-list-loop.blade.php
@@ -1,7 +1,7 @@
 @foreach($publication_list as $item)
 
     <div class="col-md-6 col-lg-4">
-        <div class="blog">
+        <div class="blog @if($item->publication_price > 0) -locked @endif ">
             <a href="{{$item['publication_url_'.$lang]}}" class="blog__img">
                 <img src="{{$item['publication_image']}}?width=360&height=250" alt="">
             </a>
