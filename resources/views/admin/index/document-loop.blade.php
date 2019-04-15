197d227f21dfa8864abbaf27feff2f65d39e674c
abdi_arman <arman.abdiyev@gmail.com>
2019-04-16 01:57:15 +0600

    commit

--- resources/views/admin/index/document-loop.blade.php
+++ resources/views/admin/index/document-loop.blade.php
@@ -4,7 +4,7 @@
 
         <div class="col-md-12 multiple-file">
             <div class="row" style="border: 2px solid #1E88E5;padding: 10px; margin: 5px">
-                <input type="hidden" name="file_url_input" value="{{$item['file_url']}}"/>
+                <input type="hidden" name="file_url_input[]" value="{{$item['file_url']}}"/>
                 <div class="col-md-4">
                     <div class="form-group">
                         <label class="control-label">Фото</label>
@@ -19,9 +19,9 @@
                     </a>
                     <div class="form-group has-danger">
                         <label class="control-label">Название файла</label>
-                        <textarea class="form-control form-control-danger" name="file_multiple_name_ru">{{$item['file_text']}}</textarea>
+                        <textarea class="form-control form-control-danger" name="file_multiple_name_ru[]">{{$item['file_text']}}</textarea>
                         <label class="control-label" style="margin-top: 23px">Отображать</label>
-                        <select class="form-control" name="file_multiple_is_show">
+                        <select class="form-control" name="file_multiple_is_show[]">
                             <option @if($item['is_show'] == 1) selected="selected" @endif>Да</option>
                             <option @if($item['is_show'] == 0) selected="selected" @endif>Нет</option>
                         </select>
